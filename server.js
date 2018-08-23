876fd711b3cfe17d0b6e662cac718d4b4874aa29
mohsen bashirzadeh <mohsen.bashirzadeh@gmail.com>
2018-08-23 22:02:46 +0430

    update

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req,res) => {res.json('app is work correctly')});
-// app.get('/', (req,res) => {db.select('*').from('users').then(data=>{res.json(data);})});
-// app.post('/signin', (req,res)=> {signin.signinControl(req,res,db,bcrypt)});
-// app.post('/register',(req,res)=> {register.registerControl(req,res,db,bcrypt)});
-// app.get('/profile/:id', (req,res)=> {profile.profileControl(req,res,db)});
-// app.put('/image',(req,res)=> {image.imageControl(req,res,db)});
-// app.post('/imageurl',(req,res)=> {image.clarifaiApi(req,res)});
+app.get('/', (req,res) => {db.select('*').from('users').then(data=>{res.json(data);})});
+app.post('/signin', (req,res)=> {signin.signinControl(req,res,db,bcrypt)});
+app.post('/register',(req,res)=> {register.registerControl(req,res,db,bcrypt)});
+app.get('/profile/:id', (req,res)=> {profile.profileControl(req,res,db)});
+app.put('/image',(req,res)=> {image.imageControl(req,res,db)});
+app.post('/imageurl',(req,res)=> {image.clarifaiApi(req,res)});
 app.listen(myPort, ()=> {	console.log(`app is running on port ${myPort}`)});
