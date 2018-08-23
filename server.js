7515abec9b4f8d05fdcf4328ada7d611aaf3bd33
mohsen bashirzadeh <mohsen.bashirzadeh@gmail.com>
2018-08-23 21:53:52 +0430

    update

--- server.js
+++ server.js
@@ -3,32 +3,32 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex');
-const register = require('./controller/register');
-const signin = require('./controller/signin');
-const profile = require('./controller/profile');
-const image = require('./controller/image');
+// const register = require('./controller/register');
+// const signin = require('./controller/signin');
+// const profile = require('./controller/profile');
+// const image = require('./controller/image');
 
 const myPort = process.env.PORT || 3000;
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    host : process.env.DATABASE_URL,
-    user : 'mbgame',
-    password : '091157',
-    database : 'smart-brain'
-  }
-});
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host : '127.0.0.1',
+//     user : 'mbgame',
+//     password : '091157',
+//     database : 'smart-brain'
+//   }
+// });
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req,res) => {db.select('*').from('users').then(data=>{res.json(data);})});
-app.post('/signin', (req,res)=> {signin.signinControl(req,res,db,bcrypt)});
-app.post('/register',(req,res)=> {register.registerControl(req,res,db,bcrypt)});
-app.get('/profile/:id', (req,res)=> {profile.profileControl(req,res,db)});
-app.put('/image',(req,res)=> {image.imageControl(req,res,db)});
-app.post('/imageurl',(req,res)=> {image.clarifaiApi(req,res)});
-app.listen(myPort, ()=> {	console.log(`app is running on port ${myPort}  and  ${process.env.DATABASE_URL} `)});
-console.log(process.env.DATABASE_URL);
+app.get('/', (req,res) => {console.log('app is work correctly')});
+// app.get('/', (req,res) => {db.select('*').from('users').then(data=>{res.json(data);})});
+// app.post('/signin', (req,res)=> {signin.signinControl(req,res,db,bcrypt)});
+// app.post('/register',(req,res)=> {register.registerControl(req,res,db,bcrypt)});
+// app.get('/profile/:id', (req,res)=> {profile.profileControl(req,res,db)});
+// app.put('/image',(req,res)=> {image.imageControl(req,res,db)});
+// app.post('/imageurl',(req,res)=> {image.clarifaiApi(req,res)});
+app.listen(myPort, ()=> {	console.log(`app is running on port ${myPort}`)});
